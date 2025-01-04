433725c5e3f99af4d581068b6ab9d44063b275f2
P1V0VAR <nikitakorshun@mail.ru>
2025-01-04 21:31:12 +0300

    first update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ToastProvider } from '@/components/providers/toaster-provider'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +24,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
