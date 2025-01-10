1a6d8360ede8a1ab0e419984e4ce30965083b9e6
P1V0VAR <nikitakorshun@mail.ru>
2025-01-10 18:57:03 +0300

    upd

--- lib/format.ts
+++ lib/format.ts
@@ -1,4 +1,4 @@
-export const formatePrice = (price: number) => {
+export const formatPrice = (price: number) => {
     return new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "USD"
